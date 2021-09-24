fad2b04ffe280eb0ecfc633043cea892b76579fc
Alexander Messier <alexander.j.messier@gmail.com>
2021-09-24 11:47:46 -0500

    Added an edit function in views.py that takes in response and hero id. Added a path for it in superheroes/urls.py. Created an edit.html that has a form with the hero's values already input into the fields. The user can change the values and the edit function in views.py will handle the POST and save the new values to the database and return to index.html after.

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -34,3 +34,19 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def edit(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    if request.method == 'POST':
+        single_hero.name = request.POST.get('name')
+        single_hero.alter_ego = request.POST.get('alter_ego')
+        single_hero.primary_ability = request.POST.get('primary')
+        single_hero.secondary_ability = request.POST.get('secondary')
+        single_hero.catch_phrase = request.POST.get('catchphrase')
+        single_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/edit.html', context)
